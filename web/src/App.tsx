6a2e73ef22a4f91285745cf077ea0407eee793db
Kim, Joohong <kimjooho@amazon.com>
2021-01-05 00:45:18 +0900

    A type-safe approach to Redux stores in TypeScript https://medium.com/@resir014/a-type-safe-approach-to-redux-stores-in-typescript-6474e012b81e
    Redux 4 + TypeScript: A type-safe approach
    https://medium.com/@resir014/redux-4-typescript-2-9-a-type-safe-approach-7f073917b803
    
    react-redux-typescript-example
    https://github.com/resir014/react-redux-typescript-example
    
    typesafe-actions
    https://github.com/piotrwitek/typesafe-actions
    
    react-redux-typescript-guide
    https://github.com/piotrwitek/react-redux-typescript-guide
    
    React Hooks in TypeScript
    https://medium.com/@jrwebdev/react-hooks-in-typescript-88fce7001d0d#:~:text=3%20min%20read-,Released%20in%20React%20v16.,explicit%20types%20must%20be%20set.
    
    Setup a React App with TypeScript, Storybook, and Crowdbotics
    https://medium.com/crowdbotics/setup-a-react-app-with-typescript-storybook-and-crowdbotics-71fb8001c99
    https://www.learnstorybook.com/intro-to-storybook/react/en/creating-addons/
    https://create-react-app.dev/docs/developing-components-in-isolation
    Storybook with ReactJS TypeScript: https://medium.com/@annycarolinegnr/storybook-with-react-typescript-1c15a1cbc26a
    
    Redux Toolkit
    https://redux-toolkit.js.org/tutorials/basic-tutorial
    
    React Redux
    https://react-redux.js.org/using-react-redux/static-typing
    
    GraphQL & React tutorial (part 1/6)
    https://www.chromatic.com/blog/graphql-react-tutorial-part-1-6/#.o54ygcruh
    
    Exploring EcmaScript Decorators
    https://medium.com/google-developers/exploring-es7-decorators-76ecb65fb841
    
    create-react-app
    https://github.com/facebook/create-react-app/tree/master/packages/cra-template
    https://www.npmjs.com/search?q=cra-template-*&page=0&perPage=20&ranking=quality

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { useForm } from "react-hook-form";
 import Amplify, { API, graphqlOperation } from "aws-amplify";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient
+} from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Observable from 'zen-observable';
 
 import './App.css';
-import { CreateClipboardInput, ListClipboardsQuery } from './API';
+import { CreateClipboardInput, ListClipboardsQuery, ListClipboardsQueryVariables, CreateClipboardMutation, CreateClipboardMutationVariables, OnCreateClipboardSubscription } from './API';
 import { createClipboard } from './graphql/mutations';
 import { listClipboards } from './graphql/queries';
+import { onCreateClipboard } from './graphql/subscriptions';
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
+
+
 
 export const gqlOp = async <
   ResultType extends {},
@@ -23,35 +33,189 @@ export const gqlOp = async <
   return data;
 };
 
+export const useAmplifyMutation = (query: string, variables?: CreateClipboardMutationVariables) => {
+  return useMutation<CreateClipboardMutation, Error, CreateClipboardMutationVariables>(async (variables) => {
+    const {data: response} = await API.graphql(graphqlOperation(query, variables)) as { data: CreateClipboardMutation};
+    return response;
+  });
+}
+
+// 'listClipboards'
+export const useAmplifyQuery = <ResultType extends {}, VariablesType extends {} = {}>(query: string, variables?: VariablesType) => {
+  // console.log('useListClipboards'+query);
+  return useQuery<ResultType, Error>(query, async () => {
+    const { data: response } = await API.graphql(graphqlOperation(query, variables)) as { data: ResultType};
+    return response;
+  });
+}
+
+// type ConfigType<VariableType extends {}> = {
+//   query: string;
+//   key: string;
+//   variables?: VariableType;
+// };
+
+// export const useSubscriptionByItself = <
+//   ItemType extends { id: string },
+//   VariablesType extends {} = {}
+// >({
+//   config,
+//   itemData,
+// }: {
+//   config?: ConfigType<VariablesType>;
+//   itemData?: ItemType;
+// } = {}) => {
+//   const [item, update] = React.useState<ItemType | undefined>(itemData);
+
+//   React.useEffect(() => {
+//     let unsubscribe;
+//     if (config) {
+//       const { query, key, variables } = config;
+//       const subscription = API.graphql(graphqlOperation(query, variables));
+//       if (subscription instanceof Observable) {
+//         const sub = subscription.subscribe({
+//           next: ({provider, value}) => {
+//             console.log({provider, value});
+//           }
+//           // next: payload => {
+//           //   try {
+//           //     const {
+//           //       value: {
+//           //         data: { [key]: item },
+//           //       },
+//           //     }: {
+//           //       value: { data: { [key: string]: ItemType } };
+//           //     } = payload;
+
+//           //     update(item);
+//           //   } catch (error) {
+//           //     console.error(
+//           //       `${error.message} - Check the key property: the current value is ${key}`
+//           //     );
+//           //   }
+//           // },
+//         });
+//         unsubscribe = () => {
+//           sub.unsubscribe();
+//         };
+//       }
+//     }
+//     return unsubscribe;
+//   }, [JSON.stringify(config)]);
+
+//   return [item];
+// };
+
+
+// export const useAmplifyQueryPromise = <ResultType extends {}, VariablesType extends {} = {}>(query: string, variables?: VariablesType) => {
+//   console.log('useListClipboards'+query);
+//   return new Promise<ResultType>((resolve, reject) => {
+//     return useQuery<ResultType, Error>(query, async () => {
+//       const response = await API.graphql(graphqlOperation(query, variables)) as { data: ResultType};
+//       return response.data;
+//     });
+//   });
+// }
+
+
 function App() {
-  const [clipboard, setClipboard] = useState<ListClipboardsQuery | undefined>();
+  // const [clipboard, setClipboard] = useState<ListClipboardsQuery | undefined>();
   const { register, handleSubmit, watch, errors } = useForm<CreateClipboardInput>();
 
-  useEffect(() =>{
-    const list = async () => {
-      try {
-        const response = await API.graphql(graphqlOperation(listClipboards)) as { data: ListClipboardsQuery};
-        console.log(response);
-        setClipboard(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    list();
-  }, []);
+  // useEffect(() =>{
+  //   const list = async () => {
+  //     try {
+  //       const response = await API.graphql(graphqlOperation(listClipboards)) as { data: ListClipboardsQuery};
+  //       console.log(response);
+  //       setClipboard(response.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   list();
+  // }, []);
+  // const queryClient = useQueryClient();
+  const {status, data, error, isFetching} = useAmplifyQuery(listClipboards );
+  // setClipboard(data as ListClipboardsQuery);
+  const clips = data as ListClipboardsQuery;
+  // console.log(clips);
   
   const mutation = async <ResultType extends {}, VariablesType extends {} = {}>(query: string, variables?: VariablesType) => gqlOp<ResultType, VariablesType>(query, variables);
 
+  const addClipboard = useMutation<CreateClipboardMutation, Error, CreateClipboardMutationVariables>(
+    async (variables: CreateClipboardMutationVariables) => {
+      const {data: response} = await API.graphql(graphqlOperation(createClipboard, variables)) as { data: CreateClipboardMutation};
+      return response;
+    },
+    {
+      // Optimistically update the cache value on mutate, but store
+      // the old value and return it so that it's accessible in case of
+      // an error
+      onMutate: async text => {
+        console.log(text);
+
+        return text;
+      },
+      // On failure, roll back to the previous value
+      onError: (err, variables, previousValue) =>
+        // queryClient.setQueryData('todos', previousValue),
+        console.log(err),
+      // After success or failure, refetch the todos query
+      onSuccess: () => {
+        // queryClient.invalidateQueries('todos')
+        console.log('success');
+      },
+    }
+  )
   const onSubmit = async (data: CreateClipboardInput) => {
     console.log({...data});
     try {
-      const response = mutation(createClipboard, {input: {...data}});
+      // const response = mutation(createClipboard, {input: {...data}});
+      const response = addClipboard.mutate({input: {...data}});
       // const response = await API.graphql(graphqlOperation(createClipboard, {input: {...data}}));
       console.log(response);
     } catch (error) {
       console.log(error);
     }
   }
+  // type ConfigType<VariableType extends {}> = {
+  //   query: string;
+  //   key: string;
+  //   variables?: VariableType;
+  // };
+  // const variables: OnCreateClipboardSubscription = {
+
+  // }
+  // const test = useSubscriptionByItself({config: {query: onCreateClipboard, key: 'response'}});
+  // console.log(test);
+  const queryClient = useQueryClient();
+  
+  // working version
+  useEffect(() => {
+    const subscription = API.graphql(graphqlOperation(onCreateClipboard));
+    console.log('subscription');
+    if (subscription instanceof Observable) {
+      subscription.subscribe({
+        next: (eventData) => {
+          console.log(eventData);
+          queryClient.setQueryData<ListClipboardsQuery | undefined>(listClipboards, (prevData) => {
+            console.log(prevData);
+            if (prevData !== undefined) {
+              if (prevData.listClipboards?.items?.some(clip => clip?.id === eventData.value.data.onCreateClipboard.id)) {
+                // throw new Error("Dup!");
+                console.log('Dup!');
+                return prevData;
+              } else {
+                prevData.listClipboards?.items?.push({...eventData.value.data.onCreateClipboard});
+                return prevData;
+              }
+            }
+            return {listClipboards: {items: [{...eventData.value.data.onCreateClipboard}], nextToken: null}} as ListClipboardsQuery;
+          })
+        }
+      });
+    }
+  }, []);
   
 
   return (
@@ -62,8 +226,9 @@ function App() {
         <input type="submit" />
       </form>
       <div>
-        {clipboard?.listClipboards?.items?.map(clip=><p key={clip?.id}>{clip?.payload}</p>)}
+        {clips?.listClipboards?.items?.map(clip=><p key={clip?.id}>{clip?.payload}</p>)}
       </div>
+      <ReactQueryDevtools initialIsOpen />
     </div>
   );
 }
